3fc17211dc80c0e515727f2bde86477e2ac1b1cc
Rohail Kamran <71363506+RohailKamran@users.noreply.github.com>
2024-10-10 11:22:58 +0500

    Removed all files

--- src/components/Logo/Logo.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import logo from '../../assets/Logo.svg'; // Adjust the path based on your file structure
-
-export const Logo = () => (
-  <div className="logo">
-    <img src={logo} alt="Company Logo" className="logo-image" />
-    <span className="logo-text">Reliance Asian</span>
-  </div>
-);
